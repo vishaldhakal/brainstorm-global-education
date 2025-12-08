361ae26a5a0d5366f0562c4b436c84ddb9b8f453
Reman <mhznremon79@gmail.com>
2025-12-08 12:18:43 +0545

    feat: add nextjs-toploader dependency and integrate it into the root layout for a page transition progress bar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NextTopLoader color="oklch(0.5841 0.1030 181.9558)" height={3} showSpinner={false} />
         <QueryProvider>
           <Header/>
           {children}
